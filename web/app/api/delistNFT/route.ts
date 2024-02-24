bd0e72d21c596cc128d5136faa8bddb95ae72c0f
banana <banana@tensor.so>
2024-02-23 20:30:04 -0400

    save state - add delist nft

---
+++ web/app/api/delistNFT/route.ts
@@ -0,0 +1,40 @@
+import { Connection } from '@solana/web3.js';
+import axios from 'axios';
+
+export async function GET(request: Request) {
+ 
+  console.log('[API] delistNFT called!');
+  const connection = new Connection(process.env.SOLANA_RPC_URL!);
+
+  const userParams = new URLSearchParams(request.url.split('?')[1]);
+  const { mint, owner } = Object.fromEntries(userParams.entries());
+  const blockhash = (await connection.getLatestBlockhash()).blockhash;
+
+  // Debugging
+  console.log('mint', mint);
+  console.log('owner', owner);
+
+  try {
+    const response = await axios.get(
+      'https://api.mainnet.tensordev.io/api/v1/tx/delist',
+      {
+        params: {
+          mint,
+          owner,
+          blockhash,
+        },
+        headers: {
+          accept: 'application/json',
+          'x-tensor-api-key': process.env.TENSOR_API_KEY,
+        },
+      }
+    );
+
+    console.log('[API] delistNFT 200 OK');
+    return new Response(JSON.stringify(response.data));
+  } catch (error) {
+
+    console.log('[API] delistNFT 200 OK');
+    return new Response(JSON.stringify(error));
+  }
+}
